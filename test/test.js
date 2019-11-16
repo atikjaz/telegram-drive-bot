1d27af14ba319f310d4c33746ee741345c9838a8
Yash Garg <ben10.yashgarg@gmail.com>
2019-11-16 07:44:58 +0000

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-var expect  = require('chai').expect;
+var assert  = require('chai').assert;
 const index = require('../index');
 
 describe('Test build', function(){
   it('App should return status fine', function(){
-    expect.equal(index(), 'Build Fine');
+    assert.equal(index(), 'Build Fine');
   });
 });
