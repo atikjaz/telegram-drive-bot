18a5c876f0de1aedf3099f53206c5ffeaa8f4c04
atikjaz <64772699+atikjaz@users.noreply.github.com>
2020-05-07 13:19:27 +0530

    Update .constants.js

--- .constants.js
+++ .constants.js
@@ -1,16 +1,16 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
-  ARIA_SECRET: 'aria2_secret',
-  ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
+  TOKEN: '1211311959:AAF2znEy3VgOMrGGSV5MMl5Vob8UTZ1PGnU',
+  ARIA_SECRET: 'torrentgd',
+  ARIA_DOWNLOAD_LOCATION: '/app/downloads)',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
-  ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
-  ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
-  GDRIVE_PARENT_DIR_ID: 'id_of_Drive_folder_to_upload_into',
-  SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  ARIA_FILTERED_DOMAINS: [''], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_FILENAMES: [''], // Files/top level directories with these substrings in the filename won't be downloaded
+  GDRIVE_PARENT_DIR_ID: '0AHhr370EIxJoUk9PVA',
+  SUDO_USERS: [-1211311959],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001125187364],	// Telegram chat IDs. Anyone in these chats can use the bot.
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
     enabled: false,   // Set this to true to use the notify functionality
-    host: 'hostname.domain',
+    host: 'https://ariamirrorbotkerala.herokuapp.com/',
     port: 80,
     path: '/botNotify'
   } 
